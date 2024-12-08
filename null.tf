d6e04be932f183b15cd5540ce10769e90db5a000
balatuluva <bala.tulva@gmail.com>
2024-12-08 20:33:04 +0000

    remote exec

--- null.tf
+++ null.tf
@@ -10,4 +10,18 @@ resource "null_resource" "cluster" {
       host = element(aws_instance.public-server.*.public_ip, count.index)
     }
   }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod 777 /tmp/userdata.sh",
+      "sudo /tmp/userdata.sh",
+      "sudo apt update",
+      "sudo apt install jq unzip -y",
+    ]
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      private_key = file("SecOps-Key.pem")
+      host = element(aws_instance.public-server.*.public_ip, count.index)
+    }
+  }
 }
