5dee81ad8703dfcdc040affc9b2a462dfdb4f1f5
balatuluva <bala.tulva@gmail.com>
2024-12-08 19:20:42 +0000

    VPC with public and private ec2s setup config for terraform using functions

--- main.tf
+++ main.tf
@@ -162,6 +162,23 @@ resource "aws_instance" "public-server" {
   EOF
 }
 
+resource "aws_instance" "private-server" {
+  count = length(var.private_cidr_block)
+  ami = "${lookup(var.amis, var.aws_region)}"
+  instance_type = "t2.micro"
+  key_name = var.key_name
+  subnet_id = "${element(aws_subnet.private-subnet.*.id, count.index+1)}"
+  vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+#  associate_public_ip_address = true
+  tags = {
+    Name = "${var.vpc_name}-Private-server-${count.index+1}"
+    Owner = local.Owner
+    costcenter = local.costcenter
+    TeamDL = local.TeamDL
+    environment = "${var.environment}"
+  }
+}
+
 #resource "aws_dynamodb_table" "state_locking" {
 #  hash_key = "LockID"
 #  name     = "dynamodb-state-locking"
